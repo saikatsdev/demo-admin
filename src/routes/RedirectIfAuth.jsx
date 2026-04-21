ee3501fabc4497c552516099fefa71b9f6eab153
Saikat Talukder <saikathosen444@gmail.com>
2026-04-21 18:57:34 +0600

    Working

--- src/routes/RedirectIfAuth.jsx
+++ src/routes/RedirectIfAuth.jsx
@@ -2,9 +2,11 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 const RedirectIfAuth = ({ children }) => {
-  const user = useSelector((s) => s.auth.user);
-  if (user) return <Navigate to="/dashboard" replace />;
-  return children;
+    const user = useSelector((s) => s.auth.user);
+
+    if (user) return <Navigate to="/dashboard" replace />;
+    
+    return children;
 };
 
 export default RedirectIfAuth;
--- src/routes/RequireAuth.jsx
+++ src/routes/RequireAuth.jsx
@@ -4,19 +4,21 @@ import { selectIsAuthenticated, clearCredentials } from '../features/auth/authSl
 import { useEffect } from 'react';
 
 const RequireAuth = () => {
-  const ok = useSelector(selectIsAuthenticated);
-  const location = useLocation();
-  const dispatch = useDispatch();
+    const ok = useSelector(selectIsAuthenticated);
 
-  useEffect(() => {
-    if (!ok) dispatch(clearCredentials());
-  }, [ok, dispatch]);
+    const location = useLocation();
+    
+    const dispatch = useDispatch();
 
-  return ok ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/login" replace state={{ from: location }} />
-  );
+    useEffect(() => {
+        if (!ok) dispatch(clearCredentials());
+    }, [ok, dispatch]);
+
+    return ok ? (
+        <Outlet />
+    ) : (
+        <Navigate to="/login" replace state={{ from: location }} />
+    );
 };
 
 export default RequireAuth;
